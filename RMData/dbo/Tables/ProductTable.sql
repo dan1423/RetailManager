3211e700cd8c955f4c01d9aee5fde07db607ab6e
dan1423 <oluwadare.daniel21@gmail.com>
2021-08-14 15:54:24 -0500

    Register DB Tables
    The tables for the register system

---
+++ RMData/dbo/Tables/ProductTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProductTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(150) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [RetailPrice] MONEY NOT NULL,
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+  
+)
